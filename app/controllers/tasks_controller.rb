a00ae3f6936c7ed95afdeb0d85dcef25597bc178
Marjori Fonte <marjorif@live.com>
2020-10-03 16:08:54 +0100

    Add task working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,20 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def add
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
+  #new always before the show/detail
+  get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#detail", as: "task"
+  post "tasks", to: "tasks#add"
 end
